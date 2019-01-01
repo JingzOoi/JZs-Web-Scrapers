414b42b54d7492a371ebc0ab5b71c1e516495101
JZ <OoiJingZhi@gmail.com>
2019-01-01 16:03:52 +0800

    now gets :orig images as of 1 Jan 2019. might replace pyperclip.paste() with sys.argv[1] in the future so people have to install less stuff. now gets multiple images from multiple levels of replies.

--- webcomic_twitter.py
+++ webcomic_twitter.py
@@ -1,72 +1,82 @@
 #! python3
-#webcomic_twitter.py: Downloads image files from Twitter posts. Max 4 images per post because Twitter.
-#usage: [1] Copy the twitter link into your clipboard.
-#       [2] Run the script. (Through powershell/cmd/terminal: python webcomic_twitter.py)
-#       [3] The image files will be saved into \temp\<postID> of the same folder.
-#modules required: bs4, requests, os, pyperclip
-#note: does not support images in replies as of 30 Dec 2018, only images from the 'main' post.
+# wcS_Twitter.py: Downloads image files off Twitter posts. 
+#                 Supports images from 'main' post and 'reply' posts.
+# Usage: [1] Copy the Twitter post link into the clipboard.
+#        [2] Run the script. [In powershell (Windows) / terminal (Mac): python webcomic_twitter.py]
+#        [3] The images will be downloaded in the subfolder .\temp\<Twitter handle>\<post ID>.
+#        [*] A metadata.txt file will be created to record the post and images info.
+# Note: Now downloads :orig images as of 1 Jan 2019. 
+
+# Additional modules required: requests, bs4, pyperclip
 
 from bs4 import BeautifulSoup
-import requests, os
-from pyperclip import paste
-import datetime
+import requests, os, datetime, pyperclip
 
-#pastes from clipboard
-url = paste()
-print(url)
+# takes in url directly from clipboard
+url = pyperclip.paste()
 
-#tests if the pasted content: [1] is a link [2] is a Twitter link
+# validates url
 try:
-    identifier = url.split('/')[2]
-    if 'twitter' not in url or 'https://' not in url:
-        print('The URL provided is not a Twitter link.\n')
+    # checks if url is splitable (i.e. not plain text)
+    validation = url.split('/')
+
+    # checks if url is a twitter link
+    if 'https://twitter.com' not in url:
+        print('URL is not a Twitter link.')
+        exit()
+
+    # checks if the link is splittable into 6 parts, as per a valid post link
+    elif len(validation) != 6: 
+        print('The URL provided is a Twitter link, but not a post link.')
         exit()
 except:
-    print('Copy the complete Twitter link onto the clipboard and try again.')
+    print('URL entered: {} \nCopy the correct link into the clipboard and try again.\n'.format(url))
     exit()
 
-#gets twitter page
-page = requests.get(url)
-r = page.raise_for_status()
-
-soup = BeautifulSoup(page.text, 'lxml')
-#specifies container if images exist in post
-try:
-    container = soup.select('.AdaptiveMediaOuterContainer')[0]
-except IndexError:
-    print('\nImage files not found. Please confirm that the Twitter post contain images to be fetched.')
-    print('Last item on clipboard: ' + url + '\n')
-    exit()
-
-#creates subfolder with the postID
-folderName = 'temp\\' + url.split('/')[-1] + ' (Twitter)'
+# creates folder based on Twitter handle and post ID in URL. Ignores if exists.
+folderName = 'temp\\@{}\\{}'.format(url.split('/')[3], url.split('/')[-1])
 os.makedirs(folderName, exist_ok = True)
-print('Subfolder with name ' + folderName + ' created.')
+print('Folder with name ' + folderName + ' created.')
+
+# count
 i = 0
+whitelist = ['permalink-in-reply-tos', 'permalink-tweet']
 
-#selects and downloads images
-for images in container.find_all('img'):
-    imageURL = images.get('src')
-    re = requests.get(imageURL)
-    re.raise_for_status()
-    i += 1
+try:
+    page = requests.get(url)
+    page.raise_for_status()
+    soup = BeautifulSoup(page.text, 'lxml')
 
-    imageName = os.path.basename(imageURL)
-    imageN = imageName.split('.')[1]
-    imageFileName = str(i) + "." + imageN
-    print('Downloading image ' + imageName + " as " + imageFileName + "...")
-    imageFile = open(os.path.join(folderName, imageFileName), 'wb')
+    for wl in whitelist: 
+        for post in soup.find_all('div', class_ = wl):
+            for container in post.select('.AdaptiveMediaOuterContainer'):
+                for images in container.find_all('img'):
+                    imageURL = images.get('src')
+                    sess = requests.Session()
+                    re = sess.get(imageURL + ':orig')
+                    re.raise_for_status()
+                    i += 1
 
-    for chunk in re.iter_content(100000):
-        imageFile.write(chunk)
-    imageFile.close()
-    print('Image ' + imageName + ' downloaded.')
+                    imageName = os.path.basename(imageURL)
+                    imageN = imageName.split('.')[1]
+                    imageFileName = str(i) + "." + imageN
 
-print('Done.\n')
+                    # downloads image files
+                    print('Downloading image ' + imageName + " as " + imageFileName + "...")
+                    with open(os.path.join(folderName, imageFileName), 'wb') as imageFile:   
+                        imageFile.write(re.content)
+                    imageFile.close()
+                    print('Image ' + imageName + ' downloaded.')
+except:
+    print('An error has occured. Check your connection and try again. \nIf this error persists, contact the developer to report this error.')
+    exit()
 
+print('Download tasks complete.\n')
+
+# creates metadata.txt in folder
 f = open(folderName + '\\metadata.txt', 'w+')
-f.write('Link: ' + url.format() + '\n')
+f.write('Link: ' + url + '\n')
 f.write('Twitter handle: @' + url.split('/')[3] + '\n')
-f.write(str(i) + ' images grabbed on datetime: ' + str(datetime.datetime.now()) + '\n')
+f.write('A total of {} image(s) grabbed on datetime: {}\n'.format(str(i), str(datetime.datetime.now())))
 
-print('Metadata file created.\n')
+print('Metadata file created. All operations complete.\n')
