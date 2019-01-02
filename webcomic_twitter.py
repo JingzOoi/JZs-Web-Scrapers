45ad57ce8a82f20ad56b57f8f017b2961fb167e3
JZ <OoiJingZhi@gmail.com>
2019-01-02 13:47:16 +0800

    moved some functions to scraperFunctions.py

--- webcomic_twitter.py
+++ webcomic_twitter.py
@@ -7,36 +7,24 @@
 #        [*] A metadata.txt file will be created to record the post and images info.
 # Note: Now downloads :orig images as of 1 Jan 2019. 
 
-# Additional modules required: requests, bs4
+# Additional modules required: requests, bs4, scraperFunctions
 
 from bs4 import BeautifulSoup
-import requests, os, datetime, sys
+import requests, os, sys, scraperFunctions
 
-# takes in url directly from clipboard
+# takes URL from first argument
 url = sys.argv[1]
 sess = requests.Session()
 # validates url
 try:
-    # checks if url is splitable (i.e. not plain text)
-    validation = url.split('/')
-
-    # checks if url is a twitter link
-    if 'https://twitter.com' not in url:
-        print('URL is not a Twitter link.')
-        exit()
-
-    # checks if the link is splittable into 6 parts, as per a valid post link
-    elif len(validation) != 6: 
-        print('The URL provided is a Twitter link, but not a post link.')
-        exit()
+    scraperFunctions.validateURL(url, 'https://twitter.com', 6)
 except:
     print('URL entered: {} \nCopy the correct link into the clipboard and try again.\n'.format(url))
     exit()
 
-# creates folder based on Twitter handle and post ID in URL. Ignores if exists.
-folderName = 'temp\\@{}\\{}'.format(url.split('/')[3], url.split('/')[-1])
-os.makedirs(folderName, exist_ok = True)
-print('Folder with name ' + folderName + ' created.')
+folder1 = url.split('/')[3]
+folder2 = url.split('/')[-1]
+folderName = scraperFunctions.createFolderPath(folder1, folder2)
 
 # count
 i = 0
@@ -60,27 +48,18 @@ try:
                     re.raise_for_status()
                     i += 1
 
-                    # attaches original file extension to new image name
-                    imageName = os.path.basename(imageURL)
-                    imageN = imageName.split('.')[1]
-                    imageFileName = str(i) + "." + imageN
+                    imageRenaming = scraperFunctions.imageRename(imageURL, i)
+                    imageNName = imageRenaming[0]
+                    imageOName = imageRenaming[1]
 
-                    # downloads image files to created folder
-                    print('Downloading image ' + imageName + " as " + imageFileName + "...")
-                    with open(os.path.join(folderName, imageFileName), 'wb') as imageFile:   
-                        imageFile.write(re.content)
-                    imageFile.close()
-                    print('Image ' + imageName + ' downloaded.')
+                    scraperFunctions.imageDownload(re, folderName, imageOName, imageNName)
 except:
     print('An error has occured. Check your connection and try again. \nIf this error persists, contact the developer to report this error.')
     exit()
 
 print('Download tasks complete. Creating metadata file.\n')
 
-# creates metadata.txt in folder
-f = open(folderName + '\\metadata.txt', 'w+')
-f.write('Link: ' + url + '\n')
-f.write('Twitter handle: @' + url.split('/')[3] + '\n')
-f.write('A total of {} image(s) grabbed on datetime: {}\n'.format(str(i), str(datetime.datetime.now())))
+authorName = url.split('/')[3]
+scraperFunctions.createMetadata(folderName, authorName, url, i)
 
 print('Metadata file created. All operations complete.\n')
