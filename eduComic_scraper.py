79f6c00b0df0bda3c3cc7f04583a3522b7a70587
JZ <OoiJingZhi@gmail.com>
2019-05-01 12:57:23 +0800

    rename to avoid collision with other modules, too generic

---
+++ eduComic_scraper.py
@@ -0,0 +1,76 @@
+from requests_html import HTMLSession
+import os, loadingBar, timeit
+
+sess = HTMLSession()
+
+class Album:
+    def __init__(self, url:str):
+
+        if len(url) <= 6:
+            self.url = f'https://nhentai.net/g/{url}/'
+        elif len(url) == 29 and 'nhentai' in url:
+            self.url = url
+        else:
+            print("Invalid link.")
+            exit()
+
+        self.magicNumber = self.url.split('/')[-2]
+        self.page = sess.get(self.url)
+        self.title = self.searchMeta(self.page)["title"]
+        self.tags = self.searchMeta(self.page)["tags"]
+        self.album = [f'https://nhentai.net{thumb.attrs["href"]}' for thumb in self.page.html.find('.gallerythumb')]
+        self.imageCount = len(self.album)
+        self.favourites = int(self.page.html.find('.nobold', first=True).text.strip('()'))
+
+    def searchMeta(self, page):
+
+        metaDict = {}
+
+        for meta in page.html.find('meta'):
+            try:
+                if meta.attrs["name"]=="twitter:title":
+                    metaDict["title"] = meta.attrs["content"]
+
+                if meta.attrs["name"]=="twitter:description":
+                    metaDict["tags"] = meta.attrs["content"]
+
+            except KeyError:
+                continue
+
+        return metaDict
+
+    def download(self):
+        dt = timeit.default_timer()
+        destinationFolder = f'temp\\eduComic\\{self.magicNumber}'
+        print(f'\nCreating folder {destinationFolder}.')
+
+        os.makedirs(destinationFolder, exist_ok=True)
+        print('Starting download operations.')
+
+        size = 0
+
+        for pageNum, image in enumerate(self.album, start=1):
+
+            img = Image(image)
+
+            with open(os.path.join(destinationFolder, img.name), 'wb') as file:
+                file.write(img.image.content)
+            size += img.size
+            loadingBar.loadingBar(self.imageCount, pageNum, message=f'{pageNum}/{self.imageCount} {img.name}')
+        
+        print('\nDownloading operations complete.\nCreating metadata file.')
+        
+        with open(os.path.join(destinationFolder, 'metadata.txt'), 'w+') as metadata:
+            metadata.write(f'Album URL: {self.url}\nTitle: {self.title}\nNumber of pages: {self.imageCount}\nTags: {self.tags}\nTotal Size: {size:,} bytes')
+
+        time = timeit.default_timer()-dt
+        
+        print(f'\nAll operations complete. \nTotal time used: {round(time, 2):,} seconds\n')
+
+class Image:
+    def __init__(self,pageURL):
+        self.srcPage = sess.get(pageURL)
+        self.link = self.srcPage.html.find('#image-container', first=True).find('img', first=True).attrs["src"]
+        self.image = sess.get(self.link)
+        self.size = int(self.image.headers["Content-Length"])
+        self.name = os.path.basename(self.link)
---
+++ zeroChan_scraper.py
@@ -0,0 +1,74 @@
+from requests_html import HTMLSession
+import os, loadingBar, timeit
+from time import sleep
+
+sess = HTMLSession()
+
+class Collection:
+    def __init__(self, tagMainURL):
+        self.url = tagMainURL
+        self.imageList = self.loop()
+        self.imageCount = len(self.imageList)
+        self.tag = self.url.split('/')[-1].replace('+',' ')
+
+    def loop(self, pageNum:int = 10):
+        
+        totalList = []
+        
+        for i in range(1, 1+pageNum):
+            page = sess.get(f'{self.url}?p={i}')
+            tempList = [f'https://zerochan.net{image.find("a", first=True).attrs["href"]}' for image in page.html.find('#thumbs2 li') if 'register' not in image.find('a', first=True).attrs["href"]]
+            totalList.extend(tempList)
+
+        return totalList
+
+    def download(self, pageNum:int = 10):
+
+        dt = timeit.default_timer()
+
+        totalList = self.loop(pageNum)
+        print(f'\n{len(totalList)} images found.')
+
+        destinationFolder = f'temp\\zeroChan\\{self.tag}'
+        print(f'\nCreating folder {destinationFolder}.')
+
+        os.makedirs(destinationFolder, exist_ok=True)
+        print('Starting download operations.')
+
+        size = 0
+
+        for num, image in enumerate(totalList, start=1):
+
+            img = Image(image)
+
+            with open(os.path.join(destinationFolder, f'[{num}] - {img.name}'), 'wb') as file:
+                file.write(img.image.content)
+
+            size += img.size
+
+            loadingBar.loadingBar(len(totalList), num, message=f'{num}/{len(totalList)} {img.name}')
+            sleep(img.time)
+
+        print('\nDownloading operations complete.\nCreating metadata file.')
+        
+        with open(os.path.join(destinationFolder, 'metadata.txt'), 'w+') as metadata:
+            metadata.write(f'Album URL: {self.url}\nTag: {self.tag}\nNumber of images: {len(totalList)}\nTotal Size: {size:,} bytes')
+
+        time = timeit.default_timer()-dt
+        
+        print(f'\nAll operations complete. \nTotal time used: {round(time, 2):,} seconds\n')
+            
+    
+class Image:
+    def __init__(self, imagePageURL):
+        self.url = imagePageURL
+        self.page = sess.get(self.url)
+        self.id = self.url.split('/')[-1]
+        try:
+            self.link = self.page.html.find('.preview', first=True).attrs["href"]
+        except:
+            self.link = self.page.html.find('#large img', first=True).attrs["src"]
+        self.image = sess.get(self.link)
+        self.name = f'{self.id}.{os.path.basename(self.link).split(".")[-1]}'
+        self.size = int(self.image.headers["Content-Length"])
+        self.time = self.image.elapsed.total_seconds()
